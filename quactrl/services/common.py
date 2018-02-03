fd438e6736312266b73d194e6d435d4aa46210fd
Salvador Ruiz <salvador.ruiz.r@gmail.com>
2018-02-03 21:43:11 +0100

    Created a generic common service

---
+++ quactrl/services/common.py
@@ -0,0 +1,186 @@
+from threading import Thread, Event
+from queue import Queue
+from quactrl.domain import get_component
+
+
+class InsertItemError(Exception):
+    pass
+
+
+class IncorrectOperationInputs(Exception):
+    pass
+
+
+class MethodError(Exception):
+    pass
+
+
+class OneItemFlowService:
+    def __init__(self, environment):
+        super().__init__()
+        dal = environment.dal
+
+        self.interrupt_event = Event()
+        self.controller = environment.controller
+        self.main_queue = Queue()
+        self.generator = PullRunner(destination=self.main_queue,
+                                    interrupt_event=self.interrupt_event
+                                    )
+        self.generator.adapter = ProcessAdapter(
+            self.generator, dal,
+            to_node_key=environment.location_key,
+            from_node=None
+            )
+        self.operation = PullRunner(origin=self.main_queue,
+                                    controller=self.controller,
+                                    interrupt_event=self.interrupt_event
+                                    )
+        self.operation.adapter = ProcessAdapter(
+            self.operation, dal,
+            from_node_key=environment.location_key,
+            name=environment.operation_name
+            )
+
+    def enter_item(self, item_data, responsible):
+        tokens = self.operation.adapter.get_tokens(item_data)
+        if tokens is None:
+            if self.generator.is_loaded():
+                self.generator.origin.put(((item_data, 1.0), responsible))
+            else:
+                self.controller.notify_error(
+                    InsertItemError(),
+                    item_data
+                    )
+        else:
+            self.operation.source.put((tokens, responsible))
+
+    def start(self):
+        if self.generator.is_loaded():
+            self.generator.start()
+        self.operation.start()
+
+    def stop(self):
+        self.operation.origin.put(None)
+        if self.generator.has_path():
+            self.generator.origin.put(None)
+
+    def interrupt(self):
+        self.interrupt_event.set()
+
+
+class MethodRepo:
+    _methods = {}
+
+    @classmethod
+    def get(cls, key):
+        if key not in cls._methods:
+            method = get_component(key)
+            if method is None:
+                return None
+            cls._methods[key] = method
+
+        return cls._methods[key]
+
+
+class PullRunner(Thread):
+    def __init__(self, origin=None, destination=None, interrupt_event=None,
+                 controller=None):
+        super().__init__()
+        self.interrupt_event = interrupt_event
+        self.controller = controller
+
+        self.devices = None
+        self.method = None
+
+        self.steps = None
+        self.origin = Queue() if origin is None else origin
+        self.destination = Queue() if destination is None else destination
+        self.adapter = None
+        self.path = None
+
+    def path_is_loaded(self):
+        return self.path is not None
+
+    def load_process(self):
+        self.steps = []
+        if self.path:
+            self.method = MethodRepo.get(self.path.method_name)
+            for sub_path in self.path.children:
+                self.steps.append(StepRunner(sub_path))
+
+    def run(self):
+        while True:
+            _input = self.origin.get()
+            if _input is None or (
+                    self.interrupt_event is not None and
+                    self.interrupt_event.is_set()
+                    ):  # thread is finished
+                break
+            in_tokens, responsible = _input
+            if (self.path is None or
+                    not self.path.accept_tokens(in_tokens)):
+                self.adapter.set_path(in_tokens)
+                self.adapter.set_devices()
+                self.load_process()
+
+            if self.path is None:
+                self.notify_error(IncorrectOperationInputs(), tokens=in_tokens)
+            else:
+                if self.path.children:
+                    for step in self.steps:
+                        step.execute(in_tokens)
+                        if (self.interrupt_event is not None and
+                                self.interrupt_event.is_set()):
+                            self.adapter.commit()
+                            break
+                self.execute(in_tokens)
+                self.path.process_tokens(responsible)
+                self.destination.put(self.process.out_tokens)
+                self.controller.notify('process_finished', self.path)
+            self.adapter.commit()
+        self.adapter.close()
+
+    def execute(self, in_tokens):
+        if self.method:
+            self.method(self.path, in_tokens, self.devices,
+                        self.controller)
+
+
+class ProcessAdapter:
+    """Adapter between dinamic ProcessRunner and Static Path on Domain"""
+    def __init__(self, process_runner, dal, **path_args):
+        self.process_runner = process_runner
+        self.dal = dal
+        self.session = self.process_runner.session
+        self.path_args = path_args
+        self._session = self.dal.Session()
+
+    def set_path(self, in_items):
+        """Load a path compatible with the in_items, return None otherwise"""
+        self.process_runner.path = self.dal.get_path(self.path_args, self.session)
+
+    def set_devices(self):
+        if self.process_runner.path:
+            self.process_runner.devices = self.dal.do.get_devices_by_location(
+                self.process_runner.path.from_node
+            )
+    def find_inputs(self, **item_args):
+        inputs = self.dal.do.get_inputs(self.path, item_args, self.session)
+        return inputs
+
+    def commit(self):
+        self._session.commit()
+
+    def close(self):
+        self._session.close()
+
+
+class StepRunner:
+    """Sync version of path"""
+    def __init__(self, process):
+        self.process = process
+        self.method = MethodRepo.get(self.method_name)
+
+    def execute(self, in_resources, devices, controller=None):
+        if self.method:
+            self.method(self.process, in_resources, devices, controller)
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,24 @@
+from unittest.mock import patch
 import pytest
 
 current = pytest.mark.current
 
 
 class TestWithPatches:
-    pass
+    def create_patches(self, definitions):
+        self.patch = {}
+        self._patchers = []
+        for definition in definitions:
+            patcher = patch(definition)
+            name = definition.split('.')[-1]
+            self.patch[name] = patcher.start()
+            self._patchers.append(patcher)
+            setattr(self, name, self.patch[name])
+
+    def teardown_method(self, method):
+        for patcher in self._patchers:
+            patcher.stop()
+
 
 
 class DataTest:
