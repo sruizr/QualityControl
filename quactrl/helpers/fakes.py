01ac955fea26dfcc79704fa4b2ccf21cc5c30f5a
Salvador Ruiz <salvador.ruiz.r@gmail.com>
2018-05-03 22:31:24 +0200

    Created fake API REST server

--- quactrl/helpers/fakes.py
+++ quactrl/helpers/fakes.py
@@ -1,80 +1,60 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+import copy
 
 
-routes = {
-    'GET': {
-        '/tests/1/parts': {
-            'key': '12364', 'description': 'CamRegis'
-        }
-
-    }
-}
-
-
-class RequestHandler(BaseHTTPRequestHandler):
-    routes = {}
+class MockRestHandler(BaseHTTPRequestHandler):
+    routes = None
 
     def do_GET(self):
-        # Send response status code
-        self.send_response(200)
-
-        # Send headers
-        self.send_header('Content-type','application/json')
-        self.end_headers()
-
-        routes = self.routes['GET']
-        if self.path in routes.keys():
-            out = routes[self.path]
-        else:
-            out = {}
-
-        # Send message back to client
-        out = json.dumps(out)
-        # Write content as utf-8 data
-        self.wfile.write(bytes(out, "utf8"))
-
-        return
-
+        self._handle_path('GET')
 
     def do_PUT(self):
-                # Send response status code
-        self.send_response(200)
-
-        # Send headers
-        self.send_header('Content-type','application/json')
-        self.end_headers()
-
-        # Send message back to client
-        out  = {'put': [1, 12]}
-        out = json.dumps(out)
-        # Write content as utf-8 data
-        self.wfile.write(bytes(out, "utf8"))
-
-        return
+        self._handle_path('PUT')
 
     def do_PATCH(self):
-        self.send_response(200)
+        self._handle_path('PATCH')
 
-        # Send headers
-        self.send_header('Content-type','application/json')
+    def do_DELETE(self):
+        self._handle_path('DELETE')
+
+    def do_POST(self):
+        self._handle_path('POST')
+
+    def _handle_path(self, method):
+        routes = self.routes[method]
+        if self.path in routes.keys():
+            answer = routes[self.path]
+            if type(answer) is dict and 'many' in answer.keys():
+                answers = answer['many']
+                requests = answer.get('requests', 0)
+                index = requests % len(answers)
+                answer = answers[index]
+            answer = copy.deepcopy(answer)
+            status_code = answer.pop('status_code', 200)
+            self._handle_res(status_code, answer)
+        else:
+            self._handle_res(404, {'error_at': method + ':' + self.path})
+
+    def _handle_res(self, status_code, json_data):
+        self.send_response(status_code)
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
 
         # Send message back to client
-        out  = {'patch': [1, 12]}
-        out = json.dumps(out)
+        out = json.dumps(json_data)
         # Write content as utf-8 data
         self.wfile.write(bytes(out, "utf8"))
 
         return
 
+class Server:
+    def __init__(self, host='127.0.0.1', port=8000):
+        self.server_address = (host, port)
 
-def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('127.0.0.3', 8000)
-    httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
+    def start(self, handler_class):
+        self.httpd = HTTPServer(server_address, handler_class)
+        self.httpd.serve_forever()
 
-
-print("Haciendo")
-RequestHandler.routes = routes
-run(handler_class=RequestHandler)
+    def stop(self):
+        self.httpd.shutdown()
