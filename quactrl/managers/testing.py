94079cf71644e90d4443659e9099f4633715900b
Salvador Ruiz <salvador.ruiz.r@gmail.com>
2018-06-26 23:25:00 +0200

    Reviewing testing manager

--- quactrl/managers/testing.py
+++ quactrl/managers/testing.py
@@ -6,6 +6,7 @@ from quactrl.managers.devices import DeviceManager
 from quactrl.managers import Event
 from quactrl.domain.flows import Creation
 from quactrl.domain.flows import Test, Check
+from quactrl.helpers.managers import StoppableThread
 
 
 class TestManager:
@@ -23,9 +24,18 @@ class TestManager:
                 else tester.test
                 for tester in self.testers]
 
-    def set_database(self, connection_string):
-        """Connects database"""
-        dal.connect(connection_string)
+    def connect(self, **kwargs):
+        """Connect to database"""
+        dal.connect(**kwargs)
+
+
+    def setup(self, location_key, process_key, cavities=1,
+              till_first_failure=True):
+        """Setup Manager with running variables"""
+        self.testers = [None] * cavities
+        self._tff = till_first_failure
+        self.location_key = location_key
+        self.process_key = process_key
 
     def go_to(self, location):
         """Load all devices from location"""
@@ -48,24 +58,24 @@ class TestManager:
 
         index = cavity - 1
         if self.testers[index] is None:
-            tester = Tester(self)
+            tester = Tester(self, till_first_failure=self._tff)
             tester.start()
             self.testers[index] = tester
 
         return tester
 
     def stop(self, cavity=None):
-        pending_pars = []
+        pending_orders = []
 
         if cavity is None:
             for tester in self.testers:
                 if tester:
-                    pending_pars.extend(tester.stop())
+                    pending_orders.extend(tester.stop())
         else:
             tester = self.testers[cavity-1]
             if tester:
-                pending_pars.extend(tester.stop())
-        return pending_pars
+                pending_orders.extend(tester.stop())
+        return pending_orders
 
 
 class Feedback(threading.Event):
@@ -83,37 +93,39 @@ class Feedback(threading.Event):
         self.set()
 
 
-class Tester(threading.Thread):
+class Tester(StoppableThread):
     def __init__(self, manager, cavity=1):
         super().__init__()
-        self.location = manager.location
+        self.location = qry.get_location(manager.location_key)
         self.dev_manager = manager.dev_manager
-        self.events = manager.events
-        self.orders = Queue()
-        self.responsible = None
-        self.stop_signal = False
-        self.cancel_signal = False
-        self.control_plan = None
-        self.responsible = None
-        self.open_check = None
+        self.ttf = manager.ttf
         self.cavity = cavity
 
-    def run(self):
-        while not self.stop_signal:
-            order = self.orders.get()
-            if order is not None:
-                self.process(order)
+        self.events = Queue()
+        self.orders = Queue()
+        self.responsible = None
+        self.cancel_signal = False
+        self.control_plan = None
+        self.open_check = None
+
+    def loop(self):
+        """One cycle loop for tester"""
+        order = self.orders.get()
+        if order is not None:
+            self.process(order)
 
     def stop(self):
-        pending_pars = []
-        self.stop_signal = True
+        """Stop thread and return unprocessed orders"""
+        pending_orders = []
 
         if self.orders.qsize() == 0:
             self.orders.put(None)
         else:
             while not self.orders.empty():
-                part_info, _, _ = self.orders.get()
-                pending_pars.append(part_info)
+                pending_orders.append(self.orders.get())
+
+        super().stop()
+        return pending_orders
 
     def cancel_test(self):
         self.cancel_signal = True
@@ -122,10 +134,10 @@ class Tester(threading.Thread):
             self.open_check.finished.set()
 
     def _get_or_create_part(self, part_info, location):
-        pass
+        self.session
 
     def process(self, order):
-        session = dal.Session()
+        self.session = session = dal.Session()
 
         part_info, responsible_key, test_pars = order
 
@@ -137,19 +149,19 @@ class Tester(threading.Thread):
         test = Test(self.control_plan, self.responsible)
         test.set_input('part', part)
         session.add(test)
-        self.events.put(Event('start', test, cavity=self.cavity))
+        self.events.put(Event('start_test', test, cavity=self.cavity))
 
         for control in self.control_plan.steps:
             check = Check(test, control, self.responsible)
             session.add(check)
-            self.events.put(Event('start', check, cavity=self.cavity))
+            self.events.put(Event('start_check', check, cavity=self.cavity))
 
             check.prepare()
             check.dev_manager = self.dev_manager
             check.tester = self
 
             if self.cancel_signal:
-                self.events.put(Event('cancel', check, cavity=self.cavity))
+                self.events.put(Event('cancel_test', check, cavity=self.cavity))
                 check.result = 'cancelled'
             else:
                 try:
@@ -167,10 +179,10 @@ class Tester(threading.Thread):
                     self.cancel_signal = True
 
                 if check.result != 'cancelled':
-                    self.events.put(Event('finish', check,
+                    self.events.put(Event('finish_test', check,
                                           cavity=self.cavity))
                 else:
-                    self.events.put(Event('cancel', check, cavity=self.cavity))
+                    self.events.put(Event('cancel_test', check, cavity=self.cavity))
 
                 check.terminate()
 
@@ -182,7 +194,7 @@ class Tester(threading.Thread):
         self.session.add(test)
         self.session.commit()
 
-        self.events.put(Event('finish', test, cavity=self.cavity))
+        self.events.put(Event('finish_test', test, cavity=self.cavity))
         self.cancel_signal = False
 
     def request_feedback(self, template):
@@ -198,15 +210,17 @@ class Tester(threading.Thread):
 
     def set_responsible_by(self, key):
         if self.responsible is None or self.responsible.key != key:
-            self.responsible = dal.get_responsible_by(key=key)
+            self.responsible = qry.get_responsible_by(key=key)
 
     def set_control_plan_for(self, part):
         if (
                 self.control_plan is None
-                or not self.control_plan.has_output(part.resource)
+                or not self.control_plan.has_output(part.part_model)
         ):
-            self.control_plan = dal.get_control_plan_by(self.location,
-                                                        part.resource)
+            self.control_plan = qry.get_control_plan_by(
+                location=self.location,
+                process=self.process,
+                part_model=part.part_model)
 
     def cancel(self):
         self.cancel_signal = True
