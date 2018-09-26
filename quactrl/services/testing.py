cc7ce437435380083622ac7b89ee130b40011a5a
Salvador Ruiz <salvador.ruiz.r@gmail.com>
2018-09-26 23:47:25 +0200

    Refactoring Testing Service

--- quactrl/services/testing.py
+++ quactrl/services/testing.py
@@ -1,28 +1,24 @@
 import threading
+import sys
 from queue import Queue
-from quactrl.domain.persistence import dal
-import quactrl.domain.queries as qry
-from quactrl.managers.devices import DeviceManager
-from quactrl.managers import Event, Manager, Feedback
-from quactrl.domain.flows import Creation
-from quactrl.domain.flows import Test, Check
-from quactrl.helpers.managers import StoppableThread
-from quactrl.domain.items import Part
-from quactrl.domain.resources import PartModel
+import quactrl.models.operations as ops
+import quactrl.models.quality as qua
+from quactrl.models.devices import DeviceContainer
 
 
 class NotFoundPart(Exception):
     pass
 
 
-class TestManager(Manager):
+class TestingService:
     """Create and manage tests"""
-    def __init__(self):
-        Manager.__init__(self)
+    def __init__(self, persistence):
+        self.persistence = persistence
+
         self.events = []
-        self.testers = []
-        self.dev_manager = DeviceManager()
+        self.inspectors = []
         self.location_key = None
+
 
     @property
     def cavities(self):
@@ -31,234 +27,170 @@ class TestManager(Manager):
     @property
     def tests(self):
         """List tests by cavity"""
-        return [None if tester is None
+        return [None if self.inspectors is None
                 else tester.test
                 for tester in self.testers]
 
-    def connect(self, **kwargs):
-        """Connect to database"""
-        dal.connect(**kwargs)
+#     def setup(self, location_key, process_key, cavities=1,
+#               create_part=False, till_first_failure=True):
+#         """Setup Manager with running variables"""
+#         self.tff = till_first_failure
+#         self.location_key = location_key
+#         self.process_key = process_key
+#         self.location_key = location_key
+#         self.create_part = create_part
 
-    def setup(self, location_key, process_key, cavities=1,
-              create_part=False, till_first_failure=True):
-        """Setup Manager with running variables"""
-        self.tff = till_first_failure
-        self.location_key = location_key
-        self.process_key = process_key
-        self.location_key = location_key
-        self.create_part = create_part
+#         self.dev_manager.load_devs_from(location_key)
 
-        self.dev_manager.load_devs_from(location_key)
+#         for tester in self.testers:
+#             if tester.is_alive():
+#                 tester.stop()
 
-        for tester in self.testers:
-            if tester.is_alive():
-                tester.stop()
-
-        self.testers = [Tester(self) for _ in range(cavities)]
-        self.events = [[] for _ in range(cavities)]
+#         self.testers = [Tester(self) for _ in range(cavities)]
+#         self.events = [[] for _ in range(cavities)]
 
 
-    def start_test(self, part_info, responsible_key, cavity=1):
-        """Start test from part_information, responsible and other process parameters"""
-        tester = self.testers[cavity - 1]
-        tester.orders.put((part_info, responsible_key))
+#     def start_test(self, part_info, responsible_key, cavity=1):
+#         """Start test from part_information, responsible and other process parameters"""
+#         tester = self.testers[cavity - 1]
+#         tester.orders.put((part_info, responsible_key))
 
-    def stop(self, cavity=None):
-        """Stop a concrete tester or all testers"""
-        pending_orders = []
+#     def stop(self, cavity=None):
+#         """Stop a concrete tester or all testers"""
+#         pending_orders = []
 
-        if cavity is None:
-            for tester in self.testers:
-                if tester:
-                    pending_orders.extend(tester.stop())
+#         if cavity is None:
+#             for tester in self.testers:
+#                 if tester:
+#                     pending_orders.extend(tester.stop())
 
-            self.testers = [Tester(self) for _ in range(self.cavities)]
-        else:
-            tester = self.testers[cavity - 1]
-            if tester:
-                pending_orders.extend(tester.stop())
-            self.testers[cavity - 1] = Tester(self)
-        return pending_orders
+#             self.testers = [Tester(self) for _ in range(self.cavities)]
+#         else:
+#             tester = self.testers[cavity - 1]
+#             if tester:
+#                 pending_orders.extend(tester.stop())
+#             self.testers[cavity - 1] = Tester(self)
+#         return pending_orders
 
-    def notify(self, info, cavity=1):
-        """Transfer notification from client to tester"""
-        self.testers[cavity - 1].notify(info)
+#     def notify(self, info, cavity=1):
+#         """Transfer notification from client to tester"""
+#         self.testers[cavity - 1].notify(info)
 
-    def download_events(self, cavity=None):
-        """Get events from tester and store them to manager.events"""
-        events = []
+#     def download_events(self, cavity=None):
+#         """Get events from tester and store them to manager.events"""
+#         events = []
 
-        if cavity is None:
-            for _cavity in range(self.cavities):
-                events.append(self.download_events(_cavity + 1))
-        elif self.testers[cavity - 1]:
-                events = self.testers[cavity - 1].empty_events()
-                self.events[cavity - 1].extend(events)
-        return events
+#         if cavity is None:
+#             for _cavity in range(self.cavities):
+#                 events.append(self.download_events(_cavity + 1))
+#         elif self.testers[cavity - 1]:
+#                 events = self.testers[cavity - 1].empty_events()
+#                 self.events[cavity - 1].extend(events)
+#         return events
 
 
-class Tester(StoppableThread):
-    def __init__(self, manager, cavity=1):
-        super().__init__()
-
-        # Inherited properties from manager
-        self.location = qry.get_location(manager.location_key)
-
-        self.process = qry.get_process_by(manager.process_key)
-        self.dev_manager = manager.dev_manager
-
-        self.tff = manager.tff
-        self.create_part = manager.create_part
-
+class Inspector(threading.Thread):
+    """Inspector of one unique part sharing some devices on a location
+    """
+    def __init__(self, database, dev_container, location_key,
+                 cavity=None, tff=True):
+        """Args:
+        database(Container): Persistence layer container of providers
+        dev_container(Container): Container of devices
+        location_key: where is located the test station and all its devices
+        cavity: cavity number, None is the station is not multicavity
+        tff(Boolean): Till first failure, stops test when first failure is found
+        """
+        name = 'Inspector'
+        if cavity is not None:
+            name += '_{}'.format(cavity)
+        super().__init__(name=name)
+        self.db = database
+        self.dev_container = dev_container
+        self.location = self.db.Locations().get_by_key(location_key)
         self.cavity = cavity
+        self.tff = tff
 
-        self.events = Queue()
+        # Inputs and Outputs of inspector
         self.orders = Queue()
+        self.events = Queue()
 
+        # Batch variables
         self.responsible = None
-        self.open_check = None
-        self.cancel_signal = False
-        self.control_plan = None
-        self.feedback = None
-        self.test = None
-        self._current_check = None
+        self.part_model = None
+        self.route = None
 
-    def loop(self):
-        """One cycle loop for tester"""
-        order = self.orders.get()
-        if order is not None:
-            self.process_test(order)
+        self._stop_event = threading.Event()
+
+    def set_responsible(self, responsible_key):
+        """Loads responsible if it has changed
+        """
+        if self.responsible is None or self.responsible.key != responsible_key:
+            self.responsible = self.db.Persons().get_by_key(responsible_key)
+
+    def set_route_for(self, part_number):
+        if (
+                self.part_model is None
+                or self.part_model.part_number != part_number):
+            self.part_model = self.db.PartModels().get_by_part_number(
+                part_number)
+
+        if (
+                self.route is None
+                or self.part_model in
+                self.route.inputs['part_group'].part_models):
+            self.route = self.db.Routes().get_by_part_model(self.part_model)
+
+    def run(self):
+        """Thread activation processing order by order"""
+        while not self._stop_event.is_set():
+            order = self.orders.get()
+            if order is None:
+                self.orders.task_done()
+                break
+            else:
+                self.process_test(order)
+                self.orders.task_done()
+
+    def process_test(self, order):
+        """Process a full test  from an order
+        """
+        part_info, responsible_key = order
+        part = self.db.Parts().get_or_create(
+            part_info['part_number'], part_info['tracking'],
+            self.location)
+
+        self.set_responsible_by(responsible_key)
+        self.set_route_for(part)
+
+        self.test = test = self.route.create_operation(self.responsible)
+        self.db.Tests().add(test)
+
+        test.start(subject=part, observer=self,
+                   dev_container=self.dev_container,
+                   cavity=self.cavity, tff=self.service.tff)
+        try:
+            test.walk()
+            test.execute()
+            test.close()
+        except Exception as e:
+            self.update('exception', e)
+            test.cancel()
+        finally:
+            self.db.Session().commit()
+
+    def update(self, obj, message, *args):
+        """Receive from test notications of states
+        """
+        self.events.put((obj, message, *args))
 
     def stop(self):
         """Stop thread and return unprocessed orders"""
         pending_orders = []
 
         if self.orders.qsize() != 0:
-
             while not self.orders.empty():
                 pending_orders.append(self.orders.get())
-        self.stop_event.set()
         self.orders.put(None)
 
         return pending_orders
-
-    def cancel_test(self):
-        self.cancel_signal = True
-        if self.open_check is not None:
-            self.open_check.result = 'cancelled'
-            self.open_check.finished.set()
-
-    def get_or_create_part(self, part_info):
-        part_number = part_info.get('part_number')
-        serial_number = part_info['serial_number']
-        part = qry.get_part(part_number=part_number, location=self.location,
-                            serial_number=serial_number)
-        if part is None and self.create_part:
-            creation = Creation(self.responsible)
-            part_model = qry.get_part_model_by(part_number)
-            part = Part()
-            part.resource = part_model
-            part.tracking = serial_number
-            part.qty = 1.0
-            creation.run(self.location, part)
-
-        return part
-
-    def process_test(self, order):
-        session = dal.Session()
-
-        part_info, responsible_key = order
-
-        part = self.get_or_create_part(part_info, self.location)
-
-        self.set_responsible_by(responsible_key)
-        self.set_control_plan_for(part)
-
-        self.test = test = self.control_plan.create_flow(self.responsible)
-        test.start(part=part)
-        session.add(test)
-        test.tester = self
-
-        self._current_check = None
-        for control in self.control_plan.steps:
-            if self.cancel_signal:
-                test.cancel()
-                break
-
-            if control.pars.get('pre_feedback'):
-                data = control.pars.get('pre_feedback')
-                self.request_feedback(**data)
-
-            self._current_check = check = control.create_flow(test,
-                                                              self.responsible)
-            check.tester = self
-            check.dev_manager = self.dev_manager
-
-            session.add(check)
-            check.run()
-
-            if control.pars.get('post_feedback'):
-                data = control.pars.get('post_feedback')
-                self.request_feedback(**data)
-
-        test.execute()
-        test.terminate()
-
-        session.commit()
-
-        self._current_check = None
-        self.cancel_signal = False
-
-    def update(self, obj):
-        """Recieve notifications from observables and record it as events"""
-        _type = {'Test': 'test', 'Check': 'check'}
-
-        finished_states = ['ok', 'incorrect', 'ng', 'suspicious', 'nok']
-        if obj.state in finished_states:
-            signal = 'finished'
-        else:
-            signal = obj.state
-
-        signal = '{}_{}'.format(_type[obj.__class__.__name__], signal)
-        self.events.put(Event(signal, obj))
-
-    def request_feedback(self, message, answer_fields=None):
-        self.feedback = Feedback(message, answer_fields)
-
-        self.events.put(Event('feedback_requested', self.feedback))
-
-        # Feedback is waiting till is answered by other instance
-        self.feedback.wait()
-
-        self.events.put(Event('feedback_answered', self.feedback))
-        return self.feedback.data
-
-    def answer_feedback(self, data):
-        self.feedback.answer(data)
-
-    def set_responsible_by(self, key):
-        if self.responsible is None or self.responsible.key != key:
-            self.responsible = qry.get_responsible_by(key=key)
-
-    def set_control_plan_for(self, part):
-
-        if (
-                self.control_plan is None
-                or not self.control_plan.validate_resource(part.part_model)
-        ):
-            self.control_plan = qry.get_control_plan_by(
-                location=self.location,
-                process=self.process,
-                part_model=part.part_model)
-
-    def cancel_test(self):
-        self.cancel_signal = True
-        if self._current_check is not None and self._current_check.state == 'ongoing':
-            """There is a check with a thread started"""
-            self._current_check.cancel()
-
-    def empty_events(self):
-        events = []
-        for _ in range(self.events.qsize()):
-            events.append(self.events.get())
-        return events
