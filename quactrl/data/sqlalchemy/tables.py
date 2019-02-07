4739d5004f04a2764429d75da7ec5c3f360428ec
Salvador Ruiz <salvador.ruiz.r@gmail.com>
2019-02-07 20:31:48 +0100

    Cavities refresh even if part_manager is not active

--- quactrl/data/sqlalchemy/tables.py
+++ quactrl/data/sqlalchemy/tables.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from sqlalchemy import (Table, MetaData, Column, Integer, String, ForeignKey,
-                        DateTime, Float, Boolean)
+                        DateTime, Float, Boolean, UniqueConstraint)
 from .types import JsonEncodedDict
 from quactrl.data.sqlalchemy import metadata
 
@@ -10,7 +10,9 @@ node = Table('node', metadata,
              Column('is_a', String(50)),
              Column('key', String(15)),
              Column('name', String(50)),
-             Column('description', String(255)))
+             Column('description', String(255)),
+             UniqueConstraint('key', 'is_a', name='uix_node')
+)
 
 
 node_link = Table('node_link', metadata,
@@ -24,7 +26,9 @@ resource = Table('resource', metadata,
                  Column('key', String(15)),
                  Column('name', String(30)),
                  Column('description', String(200)),
-                 Column('pars', JsonEncodedDict))
+                 Column('pars', JsonEncodedDict),
+                 UniqueConstraint('key', 'is_a', name='uix_resource')
+)
 
 
 resource_link = Table('resource_link', metadata,
@@ -68,7 +72,9 @@ item = Table('item', metadata,
              Column('resource_id', Integer, ForeignKey('resource.id')),
              Column('is_a', String(50)),
              Column('tracking', String(150)),
-             Column('pars', JsonEncodedDict))
+             Column('pars', JsonEncodedDict),
+             UniqueConstraint('resource_id', 'tracking', name='uix_item')
+)
 
 
 token = Table(
