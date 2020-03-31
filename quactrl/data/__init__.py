82f37785aa1199049a715af75602978ff7c656d1
Salvador Ruiz <sruiz@ako.com>
2020-03-31 19:03:05 +0200

    Data class is now DataAccesslayer

--- quactrl/data/__init__.py
+++ quactrl/data/__init__.py
@@ -1,5 +1,10 @@
 from dependency_injector import providers, containers
 from quactrl.helpers import get_class
+import inspect
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class NotFoundPart(Exception):
@@ -22,99 +27,53 @@ class NotFoundNode(Exception):
     pass
 
 
-class Data(containers.DynamicContainer):
-    """Data layer with thread local session provider and singleton repositories
+class DataAccessLayer(containers.DynamicContainer):
+    """Data Access layer with thread local singleton session and repositories
     """
-    _MODELS = [
-        'hhrr.Person', 'hhrr.Role',
-        'operations.Location', 'operations.Part', 'quality.ControlPlan',
-        'products.Characteristic', 'products.PartModel', 'products.PartGroup',
-        'products.Element', 'products.Attribute', 'products.Characteristic',
-        'products.Requirement',
-        'quality.Test', 'quality.Mode',
-        'devices.DeviceModel', 'devices.Device',
-        'documents.Directory', 'documents.Form',
-        'quality.Measurement'
-    ]
 
-    def __init__(self, module_name, connection_string=None,
-                 custom_repo_path=None, **kwargs):
+    def __init__(self, module_name, connection_string=None, **kwargs):
         super().__init__()
-        self.connection_string = connection_string
-        self.module_name = 'quactrl.data.' + module_name
-
-        self.db = get_class('{}.Db'.format(self.module_name))(
+        self.db = get_class('quactrl.data.{}.Database'.format(module_name))(
             connection_string, **kwargs
         )
-
         self.Session = providers.ThreadLocalSingleton(self.db.Session)
-
-        self._repo_path = custom_repo_path
-        self._load_repos()
+        self._load_repositories('quactrl.data.' + module_name)
+        # self._load_absent_repositories()
 
     def drop_all(self):
-        """Remove all tables and schemas, including data!
+        """Remove all tables and schemas, including dal!
         """
         self.db.drop_all()
 
     def create_schema(self):
-        """Create schema on database with empty tables
+        """Create schema on dalbase with empty tables
         """
-
         self.db.create_schema()
 
-    def _find_repo_class(self, model):
-        module_name = self._repo_path if self._repo_path else self.module_name
+    def _load_repositories(self, module_name):
+        base_module = module_name.split('.')[-1]
+        module = __import__(module_name).data
+        module = getattr(module, base_module)
+        for name, obj in inspect.getmembers(module):
+            # if inspect.ismodule(obj):
 
-        # repo_path.model_path
-        RepoClass = self._get_repo_class(module_name, model)
+            #     self._load_repos('{}.{}'.format(module_e, name))
+            if inspect.isclass(obj) and name[-4:] == 'Repo':
+                Provider = providers.ThreadSafeSingleton(obj, self)
+                setattr(self, name[:-4] + 's', Provider)
+                logger.debug('Loaded repository {}'.format(name[:-4]))
 
-        if not RepoClass:  # module_path.model_path
-            RepoClass = self._get_repo_class(self.module_name, model)
+    # def _load_absent_repositories(self):
+    #     module = __import__('quactrl.data.repositories').data.repositories
+    #     for name, obj in inspect.getmembers(module):
+    #         if inspect.isclass(obj) and name[-4:] == 'Repo':
+    #             if not hasattr(self, name[:-4] + 's'):
+    #                 Provider = providers.ThreadSafeSingleton(obj, self)
+    #                 setattr(self, name[:-4] + 's', Provider)
+    #                 logger.debug('Loaded default repository {}'.format(name[:-4]))
 
-        model = model.split('.')[1]
-        if not RepoClass:  # repo_path.model_name
-            RepoClass = self._get_repo_class(module_name, model)
+    def commit(self):
+        self.Session().commit()
 
-        if not RepoClass:  # module_path.model_name
-            RepoClass = self._get_repo_class(self.module_name, model)
-
-        return RepoClass
-
-    def _get_repo_class(self, module_name, model_name):
-        repo_path = '{}.{}Repo'.format(module_name, model_name)
-
-        try:
-            RepoClass = get_class(repo_path)
-        except ImportError:  # Not found class
-            return
-
-        return RepoClass
-
-    def _load_repos(self):
-
-        for model in self._MODELS:
-            RepoClass = self._find_repo_class(model)
-            if RepoClass:
-                Provider = providers.ThreadSafeSingleton(
-                    RepoClass,
-                    data=self)
-                model_name = model.split('.')[-1]
-                setattr(self, model_name + 's', Provider)
-
-
-class Repository:
-    def __init__(self, data):
-        """Base class for a repositry class with session pattern
-        """
-        self.data = data
-
-    @property
-    def session(self):
-        return self.data.Session()
-
-    def add(self, obj):
-        self.session.add(obj)
-
-    def remove(self, obj):
-        self.session.delete(obj)
+    def rollback(self):
+        self.Session().rollback()
