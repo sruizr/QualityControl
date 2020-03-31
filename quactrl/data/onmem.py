39f16663d98ccfa9865ff4796a38bb31781c1ff0
Salvador Ruiz <sruiz@ako.com>
2020-03-31 19:10:01 +0200

    Adapted onmen dal to new repository structure

--- quactrl/data/onmem.py
+++ quactrl/data/onmem.py
@@ -1,38 +1,64 @@
 from threading import Lock
-from quactrl.data import Repository
+import quactrl.data.repositories as reps
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class Session:
-    routes = {}
-    persons = {}
-    locations = {}
-    partmodels = {}
-    attributes = {}
-    elements = {}
-    modes = {}
-    locations = {}
-    partgroups = {}
-    controlplans = {}
-    devices = {}
-    devicemodels = {}
-    tests = []
-    roles = {}
-    parts = {}
-    requirements = {}
-    characteristics = {}
-    lock = Lock()
+    _Route = {}
+    _Person = {}
+    _Location = {}
+    _PartModel = {}
+    _Attribute = {}
+    _Element = {}
+    _Mode = {}
+    _PartGroup = {}
+    _ControlPlan = []
+    _Device = []
+    _DeviceModel = {}
+    _Test = []
+    _Role = {}
+    _Part = []
+    _Requirement = {}
+    _Characteristic = {}
+    _Form = {}
+    _Directory = {}
+    _lock = Lock()
+    _Flow = []
+
+
+    def _repo(self, obj):
+        return getattr(self, '_{}'.format(obj.__class__.__name__))
+
+    def add(self, obj):
+        repo = self._repo(obj)
+        with self._lock:
+            if type(repo) is dict:
+                repo[obj.key] = obj
+            elif type(repo) is list:
+                repo.append(obj)
+
+    def delete(self, obj):
+        with self._lock:
+            repo = self._repo(obj)
+            if type(repo) is dict:
+                repo.pop(obj.key)
+            elif type(repo) is list:
+                repo.remove(obj)
 
     def commit(self):
         pass
 
     def rollback(self):
-        """It doesn't work...
-        """
         pass
 
 
-class Db:
-    def __init__(self, *args, **kwargs):
+class Database:
+    def __init__(self, connection_string, **kwargs):
+        "docstring"
         pass
 
     @property
@@ -42,164 +68,112 @@ class Db:
         return Session
 
 
-class DocuRepo(Repository):
-    pass
+class DeviceRepo(reps.DeviceRepo):
+    def get_all_from(self, location_key):
+        return [device for device in self.session._Device]
 
 
-class DirectoryRepo(Repository):
-    pass
-
-
-class KeyRepo(Repository):
-    def __init__(self, data, dict_name):
-        super().__init__(data)
-        self._repo = getattr(data.Session(), dict_name)
-
-    def add(self, obj):
-        with self.session.lock:
-            self._repo[obj.key] = obj
-
-    def remove(self, obj):
-        with self.session.lock:
-            self._repo.pop(obj.key)
-
-    def get(self, key):
-        return self._repo[key]
-
-
-class FormRepo(KeyRepo):
-    pass
-
-
-class RequirementRepo(KeyRepo):
-    def __init__(self, data):
-        super().__init__(data, 'requirements')
-
-
-class RoleRepo(KeyRepo):
-    def __init__(self, data):
-        super().__init__(data, 'roles')
-
-
-class LocationRepo(KeyRepo):
-    def __init__(self, data):
-        super().__init__(data, 'locations')
-
-
-class PersonRepo(KeyRepo):
-    def __init__(self, data):
-        super().__init__(data, 'persons')
-
-
-class ModeRepo(KeyRepo):
-    def __init__(self, data):
-        super().__init__(data, 'modes')
-
-
-class CharacteristicRepo(KeyRepo):
-    def __init__(self, data):
-        super().__init__(data, 'characteristics')
-
-
-class ElementRepo(KeyRepo):
-    def __init__(self, data):
-        super().__init__(data, 'elements')
-
-
-class AttributeRepo(KeyRepo):
-    def __init__(self, data):
-        super().__init__(data, 'attributes')
-
-
-class PartModelRepo(KeyRepo):
-    def __init__(self, data):
-        super().__init__(data, 'partmodels')
-
-
-class PartGroupRepo(KeyRepo):
-    def __init__(self, data):
-        super().__init__(data, 'partgroups')
-
-
-class DeviceModelRepo(KeyRepo):
-    def __init__(self, data):
-        super().__init__(data, 'devicemodels')
-
-
-class DeviceRepo(Repository):
-    def add(self, device):
-        session = self.data.Session()
-        with session.lock:
-            key = device.location.key
-            if key not in self.session.devices:
-                session.devices[key] = []
-            session.devices[key].append(device)
-
-    def get_all_from(self, locationkey):
-
-        return self.data.Session().devices[locationkey]
-
-
-class PartRepo(Repository):
-    def add(self, part):
-        session = self.data.Session()
-        with session.lock:
-            if part.model not in session.parts:
-                session.parts[part.model] = []
-            session.parts[part.model].append(part)
-
-    def get_by(self, partmodel, serialnumber):
-        if partmodel not in self.session.parts:
-            return
-
-        parts = self.session.parts[partmodel]
-        for part in parts:
-            if part.tracking == serialnumber:
+class PartRepo(reps.PartRepo):
+    def get_by(self, part_model, serial_number):
+        for part in self.session._Part:
+            if part.model == part_model and part.serial_number == serial_number:
                 return part
 
     def get_last_serialnumber(self, partmodel, batchnumber, pos):
-        """Retrieve the last serial number from database (if exists...)
+        """Retrieve the last serial number from dalbase (if exists...)
         """
-        if hasattr(self.session.db, 'testsaver'):
-            return self.session.db.testsaver.getmaxpartsn(partmodel, batchnumber, pos)
+        serial_numbers = [int(part.serial_number)
+                          for part in self.session._Part
+                          if (part.model == partmodel) and
+                          (part.serial_number[4:10] == batchnumber)]
+        if serial_numbers:
+            return str(max(serial_numbers))
 
 
-class ControlPlanRepo(Repository):
-    def add(self, controlplan):
-        location = controlplan.source.key
-        resources = []
-
-        for resourcemap in controlplan.outputs:
-            key = resourcemap.key
-            resources.append(key)
-
-        session = self.data.Session()
-        with session.lock:
-            for resource in resources:
-                session.controlplans[(resource, location)] = controlplan
-
-    def get_by(self, partmodel, location):
+class ControlPlanRepo(reps.ControlPlanRepo):
+    def get_by(self, part_model, location):
         """Return control plan for a partmodel on a location
         """
-        key = (partmodel.key, location.key)
-        session = self.data.Session()
-        if key in session.controlplans:
-            return self.session.db.controlplans[key]
-
-        #If there is no controlplan for specific partmodel...
-        for group in partmodel.groups:
-            key = (group.key, location.key)
-            if key in self.session.controlplans.keys():
-                return self.session.controlplans[key]
-
-
-class TestRepo(Repository):
-    def add(self, test):
-        session = self.data.Session()
-        with session.lock:
-            session.tests.append(test)
+        for control_plan in self.session._ControlPlan:
+            logger.debug('Control plan source is {}'.format(control_plan.source.key))
+            if control_plan.source == location:
+                if part_model in control_plan.outputs:
+                    return control_plan
+                else:
+                    for group in part_model.groups:
+                        if group in control_plan.outputs:
+                            return control_plan
 
 
-class MeasurementRepo(Repository):
-    def get_all(self, check):
+class MeasurementRepo(reps.MeasurementRepo):
+    def get_all_from(self, check):
         return check.measurements
+
+
+class _KeyRepo:
+    def get(self, key):
+        repo_name = '_{}'.format(self.__class__.__name__[:-4])
+        repo = getattr(self.session, repo_name)
+        # logger.debug('There are {} items on repository {}'.format(len(repo),
+        #                                                           repo_name))
+        return repo.get(key)
+
+
+class AttributeRepo(reps.AttributeRepo, _KeyRepo):
+    pass
+
+
+class ElementRepo(reps.ElementRepo, _KeyRepo):
+    pass
+
+
+class ModeRepo(reps.ModeRepo, _KeyRepo):
+    pass
+
+
+class LocationRepo(reps.LocationRepo, _KeyRepo):
+    pass
+
+
+class PartGroupRepo(reps.PartGroupRepo, _KeyRepo):
+    pass
+
+
+class PersonRepo(reps.PersonRepo, _KeyRepo):
+    pass
+
+
+class RoleRepo(reps.RoleRepo, _KeyRepo):
+    pass
+
+
+class CharacteristicRepo(reps.CharacteristicRepo, _KeyRepo):
+    pass
+
+
+class DeviceModelRepo(reps.DeviceModelRepo, _KeyRepo):
+    pass
+
+
+class RequirementRepo(reps.RequirementRepo, _KeyRepo):
+    pass
+
+
+class PartModelRepo(reps.PartModelRepo, _KeyRepo):
+    pass
+
+
+class FlowRepo(reps.FlowRepo):
+    pass
+
+
+class StepRepo(reps.StepRepo):
+    pass
+
+
+class DirectoryRepo(reps.DirectoryRepo, _KeyRepo):
+    pass
+
+
+class TestRepo(reps.TestRepo):
+    pass
